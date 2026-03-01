86a08434c6b22c2126b8daa78fab427f72b9716c
Vaishnavi <sinhavaishnavi020@gmail.com>
2026-03-01 22:32:22 +0530

    Connected to frontend using CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -4,8 +4,17 @@ from app.database import engine
 from app.models.base import Base
 from app.routes.auth import router as auth_router
 from app.routes.note import router as note_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
